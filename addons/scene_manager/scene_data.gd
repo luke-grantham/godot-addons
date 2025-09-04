19a7c75df66fcb62b095d2514543ef2a1105c1b9
luke-grantham <luke.grantham2@gmail.com>
2025-09-03 20:49:16 -0400

    Improve SceneManager

--- addons/scene_manager/scene_data.gd
+++ addons/scene_manager/scene_data.gd
@@ -7,20 +7,17 @@ signal transition_done()
 @export var scene_id: MyScenes.SceneId
 @export var entering_transition_name: String = ""
 @export var exiting_transition_name: String = ""
-@export var transition_behavior: Script
 
 var active_behavior: TransitionBehavior
 
 func run_entering_behavior():
-    if transition_behavior:
-        active_behavior = transition_behavior.new(entering_transition_name)
-        active_behavior.transition_done.connect(func(): transition_done.emit())
-        if active_behavior.has_method("execute_enter"):
-            active_behavior.execute_enter()
+    active_behavior = TransitionBehavior.new(entering_transition_name)
+    active_behavior.transition_done.connect(func(): transition_done.emit(), ConnectFlags.CONNECT_ONE_SHOT)
+    if active_behavior.has_method("execute_enter"):
+        active_behavior.execute_enter()
 
 func run_exiting_behavior():
-    if transition_behavior:
-        active_behavior = transition_behavior.new(exiting_transition_name)
-        active_behavior.transition_done.connect(func(): transition_done.emit())
-        if active_behavior.has_method("execute_exit"):
-            active_behavior.execute_exit()
+    active_behavior = TransitionBehavior.new(exiting_transition_name)
+    active_behavior.transition_done.connect(func(): transition_done.emit(), ConnectFlags.CONNECT_ONE_SHOT)
+    if active_behavior.has_method("execute_exit"):
+        active_behavior.execute_exit()
--- addons/scene_manager/scene_manager.gd
+++ addons/scene_manager/scene_manager.gd
@@ -5,6 +5,8 @@ extends Node
 
 var scene_dict: Dictionary = {}
 
+var running: bool = false
+
 func _ready() -> void:
     for d in scene_data:
         scene_dict[d.scene_id] = d
@@ -12,15 +14,22 @@ func _ready() -> void:
 func load_scene(scene_id: MyScenes.SceneId):
     var data: SceneData = scene_dict[scene_id]
 
-    if data.exiting_transition_name != "":
+    if !running and data.exiting_transition_name != "":
+        running = true
         print("SceneManager ::: Switching to %s with transition" % MyUtil.enum_name(MyScenes.SceneId, scene_id))
         data.transition_done.connect(_run_entering_behavior.bind(data), ConnectFlags.CONNECT_ONE_SHOT)
         data.run_exiting_behavior()
-    else:
+    elif !running:
         print("SceneManager ::: Switching to %s" % MyUtil.enum_name(MyScenes.SceneId, scene_id))
         get_tree().change_scene_to_packed(data.scene)
+        running = false
 
 func _run_entering_behavior(data: SceneData):
     get_tree().change_scene_to_packed(data.scene)
     if data.entering_transition_name != "":
+        data.transition_done.connect(on_done_entering, ConnectFlags.CONNECT_ONE_SHOT)
         data.run_entering_behavior()
+
+func on_done_entering():
+    running = false
+
